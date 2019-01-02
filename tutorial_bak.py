1f130c2a8dfd166f7fda29cc41a4aeb46b960775
zhuxiaojian2132 <zhuxiaojian@126.com>
2019-01-02 11:28:49 +0800

    second commit

--- tutorial_bak.py
+++ tutorial_bak.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 
 
-img=cv.imread("D:/Python_Code/VSC_Python/lena.jpg")
+img=cv.imread("E:/github_repository/VSC_Python/lena.jpg")
 cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
 cv.imshow("input image",img)
 
